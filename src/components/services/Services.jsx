30fedc68ef157aa9a1efb0bded43ffcdf6ba411e
serhatKk <musicjust0x@gmail.com>
2024-04-02 18:38:46 +0300

    Update Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -60,9 +60,11 @@ const Services = () => {
       <div className="section__deco deco__right">
         <img src={shapeOne} alt="" className='shape' />
       </div>
-      <div className="section__bg-wrapper">
-        <span className="bg__title">Hizmetler</span>
-      </div>
+      <div class="disable-select">
+                <div className="section__bg-wrapper">
+                    <span className="bg__title">Hizmetler</span>
+                </div>
+            </div>
     </section>
   );
 };
